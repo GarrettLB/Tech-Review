5ec27384ccb835f2d8db15dc1bc796290d9f1136
GLBryan <garrettleebryan@gmail.com>
2021-12-06 12:59:55 -0500

    deleting a post

--- public/js/update.js
+++ public/js/update.js
@@ -1,23 +1,44 @@
 const updateRoute = async (event) => {
     event.preventDefault();
-  
-    const id = document.querySelector('#id').value;
-    const title = document.querySelector('#titleinput').value;
-    const content = document.querySelector('#postinput').value;
-  
-    if (title && content) {
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ title, content }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+        const title = document.querySelector('#titleinput').value;
+        const content = document.querySelector('#postinput').value;
     
-  document.querySelector('#update').addEventListener('click', updateRoute);
+        if (title && content) {
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+        }
+    }
+};
+
+const deleteRoute = async (event) => {
+    event.preventDefault();
+
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+    
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+        });
+    
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+    
+document.querySelector('#update').addEventListener('click', updateRoute);
+document.querySelector('#delete').addEventListener('click', deleteRoute);
