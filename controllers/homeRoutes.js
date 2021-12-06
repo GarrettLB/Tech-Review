00d87ba521553325824ed177403adfb2c28dec04
GLBryan <garrettleebryan@gmail.com>
2021-12-06 10:31:01 -0500

    auth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,7 +50,6 @@ router.get('/posts/:id', async (req, res) => {
   }
 });
 
-
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
@@ -61,23 +60,28 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/dashboard', async (req, res) => {
-    try {
-      const postData = await Post.findAll({
-        where: {
-          user_id: req.session.user_id
-        },
-        include: [{ model: User }]
-      });
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  try {
+    const postData = await Post.findAll({
+      where: {
+      id: req.session.user_id
+      },
+      include: [{ model: User }]
+    });
   
-      const posts = postData.map(post => post.get({ plain: true }));
+    const posts = postData.map(post => post.get({ plain: true }));
   
-      res.render('titles', {
-        posts,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+    res.render('titles', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
